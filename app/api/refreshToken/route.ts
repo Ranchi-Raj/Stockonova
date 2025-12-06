e0e2fd641d86f569fd9111851edf44384e9cd73b
Aditya Raj <raj.adi792@gmail.com>
2025-12-06 20:12:18 +0530

    Hopefully the last

--- app/api/refreshToken/route.ts
+++ app/api/refreshToken/route.ts
@@ -1,5 +1,5 @@
 import {NextResponse} from "next/server";
-import DBService from "@/appwrite/db";
+// import DBService from "@/appwrite/db";
 export async function POST(req: Request){
     const {refreshToken} = await req.json();
 
@@ -7,7 +7,7 @@ export async function POST(req: Request){
         return NextResponse.json({error: "Missing refresh token"}, {status: 400});
     }   
 
-    await DBService.updateRefreshToken(refreshToken);
+    // await DBService.updateRefreshToken(refreshToken);
 
     return NextResponse.json({message: "Refresh token updated successfully"}, {status: 200});
 }
